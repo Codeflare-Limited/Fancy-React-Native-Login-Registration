f30aadf4094c9425dda9a09220aeaf04f3c1d9b7
Luckae <lawsonluke9@gmail.com>
2021-08-30 03:26:37 +0100

    First commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,3 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5b8865328ca8b29f33fccc7cabf532940cd1f3a4b3a6211bd183a017aed8882
+size 144
--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import StackNav from './Routes/StackNav';
-
-class App extends React.Component{
-  render(){
-    return(
-      <StackNav />
-    )
-  }
-}
-
- 
-export default App;
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c2a77dbcda09c364f3121fd9c8d3fc2745792c3016d1df1a57101535be0d852
+size 299
